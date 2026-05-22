893859be2e60b25dfac78a2554312cec19fc5f95
navxmane <stockmanngabriel1@proton.me>
2026-05-22 13:26:33 -0300

    Tornando o código mais parecido com o julia

--- main.py
+++ main.py
@@ -2,36 +2,48 @@ import gurobipy as gp
 import numpy as np
 import matplotlib.pyplot as plt
 
-T = 10
+T = 15
 nxu = 2
 nlam = 4
 
-P = np.eye(nxu) * 10
-Q = np.eye(nxu) * 10
+P = np.eye(nxu)
+Q = np.eye(nxu)
 
-A = np.array([[-1,0], [0,1], [1,0], [0,-1]])
+A = np.array([
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1]
+])
 b = np.array([1,1,1,1])
 
 x_init = [-5, 0]
 x_goal = [5, 0]
 m = gp.Model("NMPC")
 
-x = m.addVars(T+1, nxu, lb=-5, ub=5, name="pos")
+m.setParam("NonConvex", 2)
+m.setParam("NumericFocus", 3)
+m.setParam("BarQCPConvTol", 1e-8)
+
+x = m.addVars(T+1, nxu, lb=-6, ub=6, name="pos")
 x_bar = m.addVars(T +1, nxu, lb=-10, ub=10, name="x_bar")
-u = m.addVars(T, nxu, lb=-2, ub=2, name="u")
+u = m.addVars(T, nxu, lb=-1, ub=1, name="u")
 lam = m.addVars(T, nlam, lb=0, name='Lambda')
 mu = m.addVars(T, nxu, name='mu')
 
-obj = gp.quicksum(P[i,i] * x_bar[k,i]**2 for k in range(T) for i in range(nxu)) + gp.quicksum(Q[i,i] * u[k, i]**2 for k in range(T) for i in range(nxu))
+obj = gp.quicksum(P[i,i] * x_bar[k,i]**2 for k in range(T+1) for i in range(nxu)) + gp.quicksum(Q[i,i] * u[k, i]**2 for k in range(T) for i in range(nxu))
 m.setObjective(obj, sense=gp.GRB.MINIMIZE)
 
 for i in range(nxu):
     m.addConstr(x[0,i] == x_init[i])
     m.addConstr(x[T, i] == x_goal[i])
 
-for k in range(T):
+for k in range(T+1):
     for i in range(nxu):
         m.addConstr(x_bar[k, i] == x[k, i] - x_goal[i])
+
+for k in range(T+1):
+    for i in range(nxu):
         m.addConstr(x[k+1, i] == x[k,i] + u[k, i])
 
 
@@ -41,9 +53,14 @@ for k in range(T):
         m.addConstr(gp.quicksum(lam[k, l] * A[l, i] for l in range(nlam)) - mu[k, i] == 0)
 
 
-    m.addConstr(gp.quicksum(mu[k, i] * mu[k, i] for i in range(nxu)) <= 1)
+    m.addQConstr(gp.quicksum(mu[k, i] * mu[k, i] for i in range(nxu)) <= 1)
 
 m.optimize()
+
+if m.status == gp.GRB.OPTIMAL:
+    print("Ótimo encontrado")
+else:
+    print("Status:", m.status)
 
 x_traj = np.array([
     [x[k,0].X, x[k,1].X]
